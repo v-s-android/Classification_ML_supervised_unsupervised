b0f8f5595ad85d25b85c06477a4aece9a904d47b
Vineel Sadineni <vin.anddev@gmail.com>
2026-07-21 15:41:38 -0700

    Implement obesity risk prediction pipeline function
    Added a function to automate the obesity risk prediction pipeline, including data loading, preprocessing, model training, and evaluation.

--- multiclass_classification.py
+++ multiclass_classification.py
@@ -214,14 +214,103 @@ plt.show()
 
 # For One vs One model
 # Collect all coefficients from each underlying binary classifier
-coefs = np.array([est.coef_[0] for est in model_ovo.estimators_])
+coefs = np.array([est.coef_[0] for est in model_ovo.estimators_]) # there are lot of arrays , we take the first value and create an array of first values 
+'''
+[[ 5.26958812e-01 -2.10346113e+00  7.99657915e+00 -3.23125743e-01
+  -1.01076741e-01 -2.12634946e-01  8.65326589e-02 -1.35733425e-01
+   5.48032451e-01  1.50991813e-01  2.85700123e-01 -1.19032100e+00
+  -7.35719837e-01 -3.39310154e-01  7.67372879e-01  2.25980478e-01
+   4.28861917e-01 -2.34849885e-01 -1.94682995e-01  7.47849030e-01
+   2.28597955e-01 -1.29592814e-01  5.38003813e-01] # end of first array
+ [ 7.45066448e-01 -7.91417826e-01  4.53935137e+00 -2.47267218e-01
+  -5.00771023e-01 -1.72456665e-01 -6.79434986e-02 -1.92749959e-01
+  -2.69039086e-01  9.67800833e-01  2.29856446e-01 -7.37531871e-01
+   7.30493853e-01 -5.04824563e-02  1.36637777e-01 -1.12790975e-01
+   1.09688769e-01 -2.33972601e-01  1.28424481e-01  0.00000000e+00
+   1.60166091e-02  1.91889385e-01  9.57695805e-02] # end of second array
+...
+'''
 
 # Now take the mean across all those classifiers
-feature_importance = np.mean(np.abs(coefs), axis=0)
+feature_importance = np.mean(np.abs(coefs), axis=0) # abs converts the values into positive floats and then the mean is calculated
 
 # Plot feature importance
 plt.barh(X.columns, feature_importance)
 plt.title("Feature Importance (One-vs-One)")
 plt.xlabel("Importance")
 plt.show()
+
+'''
+Q3. Write a function obesity_risk_pipeline to automate the entire pipeline:
+
+Loading and preprocessing the data
+Training the model
+Evaluating the model
+The function should accept the file path and test set size as the input arguments.
+'''
+
+def obesity_risk_pipeline(data_path, test_size=0.2):
+    # Load data
+    df = pd.read_csv(data_path)
+
+    # Standardizing continuous numerical features
+    numerical_columns = df.select_dtypes(include = ["float64"]).columns.tolist()
+    std_scaler = StandardScaler()
+    scalered_df = std_scaler.fit_transform(df[numerical_columns])
+
+    # Converting to a DataFrame
+    std_scaler_df = pd.DataFrame(scalered_df , columns = std_scaler.get_feature_names_out(numerical_columns))
+
+    # Combining with the original dataset
+    new_df = pd.concat( [df.drop(columns=numerical_columns),std_scaler_df ], axis = 1 )
+
+    # Identifying categorical columns
+    # covert catogorical columns into numercial column values
+    categorical_columns = new_df.select_dtypes(include = ['object']).columns.tolist()
+    categorical_columns.remove('NObeyesdad') # Exclude target column
+
+     # Applying one-hot encoding
+    encoder = OneHotEncoder(sparse_output= False, drop='first') # drops the first category : ex- Blue, red, Green 
+    encoded_df = encoder.fit_transform(df[categorical_columns])
+
+    # Converting to a DataFrame
+    encoded_df_new = pd.DataFrame(encoded_df , columns = encoder.get_feature_names_out(categorical_columns))
+    prepped_df = pd.concat( [new_df.drop(columns = categorical_columns) , encoded_df_new ], axis = 1)
+
+    # Encoding the target variable
+    prepped_df['NObeyesdad'] = prepped_df['NObeyesdad'].astype('category').cat.codes
+
+    # Preparing final dataset
+    y = prepped_df['NObeyesdad']
+    X = prepped_df.drop('NObeyesdad', axis = 1)
+
+    # Splitting data
+    X_train, X_test, y_train, y_test = train_test_split( X, y, test_size= test_size, random_state=42, stratify= y )
+
+    model_ova = LogisticRegression(multi_class='ovr', max_iter=1000)
+    model_ova.fit(X_train, y_train)
+
+    y_ova_pred = model_ova.predict(X_test)
+    print(f"the model_ova accuracy is {np.round(100 * accuracy_score(y_test, y_ova_pred))}%")
+
+    model_ovo = OneVsOneClassifier(LogisticRegression(max_iter=1000))
+    model_ovo.fit(X_train, y_train)
+
+    y_ovo_pred = model_ovo.predict(X_test)
+    print(f"the model_ovo accuracy is {np.round(100 * accuracy_score(y_test, y_ovo_pred))}%")
+
+    # Training and evaluation
+    model = LogisticRegression(multi_class='multinomial', max_iter=1000)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+
+
+obesity_risk_pipeline("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/GkDzb7bWrtvGXdPOfk6CIg/Obesity-level-prediction-dataset.csv", test_size=0.2)
+
+'''
+the model_ova accuracy is 76.0%
+the model_ovo accuracy is 92.0%
+Accuracy: 0.8794326241134752
+'''
 

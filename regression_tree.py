533d7172d70228d98732f7f677404184cc213327
Vineel Sadineni <vin.anddev@gmail.com>
2026-07-22 21:35:55 -0700

    Add regression_tree.py for regression modeling exercise
    This file introduces a regression tree exercise using a NYC taxi tip dataset, outlining objectives for data preprocessing, modeling, training, and inference assessment.

---
+++ regression_tree.py
@@ -0,0 +1,12 @@
+'''
+In this exercise session you will use a real dataset to train a regression tree model. The dataset includes information about taxi tip and was collected and provided 
+to the NYC Taxi and Limousine Commission (TLC) by technology providers authorized under the Taxicab & Livery Passenger Enhancement Programs (TPEP/LPEP).
+You will use the trained model to predict the amount of tip paid.
+
+Objectives
+- Perform basic data preprocessing using Scikit-Learn
+- Model a regression task using Scikit-Learn
+- Train a Decision Tree Regressor model
+- Run inference and assess the quality of the trained models
+'''
+

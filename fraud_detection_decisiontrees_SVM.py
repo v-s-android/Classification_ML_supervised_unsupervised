b9c6acc87ac3ed4b8fc5cdcd74547fe2dbf1b888
Vineel Sadineni <vin.anddev@gmail.com>
2026-07-27 19:21:45 -0700

    Add comments for fraud detection ML models
    Add introductory comments and objectives for fraud detection models.

---
+++ fraud_detection_decisiontrees_SVM.py
@@ -0,0 +1,13 @@
+'''
+consolidate your machine learning (ML) modeling skills by using two popular classification models to identify fraudulent credit card transactions. 
+These models are: Decision Tree and Support Vector Machine. You will use a real dataset of credit card transactions to train each of these models. 
+You will then use the trained model to assess if a credit card transaction is fraudulent or not.
+
+Objectives
+
+- Perform basic data preprocessing in Python
+- Model a classification task using the Scikit-Learn Python APIs
+- Train Suppport Vector Machine and Decision Tree models using Scikit-Learn
+- Run inference and assess the quality of the trained models
+'''
+

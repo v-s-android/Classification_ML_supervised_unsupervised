f7ec23aaf4c2fce88dfdbe5be844ec0e70e1ef88
Vineel Sadineni <vin.anddev@gmail.com>
2026-09-22 14:50:24 -0700

    Initialize K-Nearest Neighbors Classifier with imports
    Added imports and installation instructions for dependencies.

---
+++ K_Nearest_Neighbors_Classifier.py
@@ -0,0 +1,24 @@
+"""
+K-Nearest Neighbors Classifier:
+
+Use K-Nearest neighbors to classify data
+Apply KNN classifier on a real world data set
+"""
+
+#install
+!pip install numpy==2.2.0
+!pip install pandas==2.2.3
+!pip install scikit-learn==1.6.0
+!pip install matplotlib==3.9.3
+!pip install seaborn==0.13.2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+

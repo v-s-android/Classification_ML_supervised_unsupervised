ddf0a01751f5fe07cdf807b5cf18247108f5c975
Vineel Sadineni <vin.anddev@gmail.com>
2026-09-22 21:22:33 -0700

    Implement KNN model evaluation for varying k values
    Added functionality to evaluate KNN model performance for different values of k and plot accuracy.

--- K_Nearest_Neighbors_Classifier.py
+++ K_Nearest_Neighbors_Classifier.py
@@ -134,5 +134,70 @@ This function is equal to the jaccard_score function. Essentially, it calculates
 """
 print("Test set Accuracy: ", accuracy_score(y_test, y_hat)) # Test set Accuracy:  0.32
 
+# When k=6
+k = 6
+knn_classifier = KNeighborsClassifier(n_neighbors=k)
+knn_model = knn_classifier.fit(X_train, y_train)
+
+y_hat = knn_model.predict(X_test)
+print("accuracy score for k=6: ", accuracy_score(y_test, y_hat)) # accuracy score for k=6:  0.31
+
+"""
+Choosing the correct value of k
+K in KNN, is the number of nearest neighbors to examine. However, the choice of the value of 'k' clearly affects the model.
+Therefore, the appropriate choice of the value of the variable k becomes an important task. The general way of doing this is to train the model
+on a set of different values of k and noting the performance of the trained model on the testing set. The model with the best value of accuracy_score is the one with the ideal value of the parameter k.
+
+Check the performance of the model for 10 values of k, ranging from 1-9. You can evaluate the accuracy along with the standard deviation of the accuracy as well to
+get a holistic picture of the model performance.
+"""
+Ks = 10
+acc = np.zeros((Ks))
+std_acc = np.zeros((Ks))
+for n in range(1,Ks+1):
+    #Train Model and Predict  
+    knn_model_n = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat = knn_model_n.predict(X_test)
+    acc[n-1] = accuracy_score(y_test, yhat)
+    std_acc[n-1] = np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+"""
+Plot the model accuracy for a different number of neighbors.
+Now, you can plot the model accuracy and the standard deviation to identify the model with the most suited value of k.
+"""
+plt.plot(range(1,Ks+1),acc,'g')
+plt.fill_between(range(1,Ks+1),acc - 1 * std_acc,acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy value', 'Standard Deviation'))
+plt.ylabel('Model Accuracy')
+plt.xlabel('Number of Neighbors (K)')
+plt.tight_layout()
+plt.show()
+
+"""
+Can you justify why the model performance on training data is deteriorating with increase in the value of k?
+
+
+When k is small (e.g., k=1), the model is highly sensitive to the individual points in the dataset. The prediction for each point is based on its closest neighbor, which can lead to highly
+specific and flexible boundaries. This leads to overfitting on the training data, meaning the model will perform very well on the training set, potentially achieving 100% accuracy. However,
+it may generalize poorly to unseen data. When k is large, the model starts to take into account more neighbors when making predictions. This has two main consequences:
+
+Smoothing of the Decision Boundary: The decision boundary becomes smoother, which means the model is less sensitive to the noise or fluctuations in the training data.
+Less Specific Predictions: With a larger k, the model considers more neighbors and therefore makes more generalized predictions, which can lead to fewer instances being classified perfectly.
+As a result, the model starts to become less flexible, and its ability to memorize the training data (which can lead to perfect accuracy with small k) is reduced.
+"""
+
+"""
+We can see that even the with the optimum values, the KNN model is not performing that well on the given data set. Can you think of the possible reasons for this?
+
+Enter you answer here
+
+Click here for the solution
+The weak performance on the model can be due to multiple reasons. 1. The KNN model relies entirely on the raw feature space at inference time.
+If the features do no provide clear boundaries between classes, KNN model cannot compensate through optimization or feature transformation.
+2. For a high number of weakly correlated features,
+the number of dimensions increases, the distance between points tend to become more uniform, reducing the discriminative power of KNN.
+3. The algorithm treats all features equally when computing distances. Hence, weakly correalted features can introduce noise or irrelevant variations 
+in the feature space making it harder for KNN to find meaningful neighbours.
+"""
+
 
 
